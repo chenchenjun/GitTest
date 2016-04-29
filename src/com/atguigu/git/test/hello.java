3e316c09212f10a5c30d9dd2d32a27c561423861
chenjunfeng2013 <junfeng2013_happy@163.com>
2016-04-29 23:55:35 +0800

    add test method

--- src/com/atguigu/git/test/hello.java
+++ src/com/atguigu/git/test/hello.java
@@ -3,7 +3,16 @@ package com.atguigu.git.test;
 public class hello {
 
 	public static void main(String[] args) {
+
+		for (int i = 0; i++ ; i < 10 ) {
+			System.out.println(i++);
+		}
 		
 		System.out.println("第一次 发送");
+
+		int j = 0;
+
+		System.out.println(i+j);
+
 	}
 }
